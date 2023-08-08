4d24e9bb70eaf58701650d4463ae136cd61ea63d
frostwebservice <103946763+frostwebservice@users.noreply.github.com>
2023-08-08 13:27:21 -0300

    update

--- src/app/pages/bookings/bookings.module.ts
+++ src/app/pages/bookings/bookings.module.ts
@@ -2,14 +2,12 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { CommonModule } from '@angular/common'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker'
 
+import { FormsModule } from '@angular/forms'
 import { ComponentsModule } from '../../components/components.module'
 import { Bookings } from './bookings.component'
 import { TranslateModule } from '@ngx-translate/core'
-import { UserService } from 'src/app/services/user.service';
-import { BranchService } from 'src/app/services/branch.service';
-import { LoaderService } from 'src/app/services/loader.service';
 
 const routes = [
 	{
@@ -22,6 +20,7 @@ const routes = [
 		declarations: [Bookings],
 		imports: [CommonModule, ComponentsModule, RouterModule.forChild(routes),
 			BsDatepickerModule,
+			FormsModule,
 			TranslateModule.forChild({ extend: true }),
 		],
 		exports: [Bookings],
